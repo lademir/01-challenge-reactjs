09c3f8b8194db343c6d5a5d62c0a7e995d2f6713
lademir <lademirjunior@hotmail.com>
2022-03-13 01:01:39 -0300

    Finishing the first Rocketseat ReactJS  challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,30 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const newId = Math.floor((Math.random() * 100) + 1);
+
+    newTaskTitle && setTasks([...tasks, {
+      id: newId,
+      title: newTaskTitle,
+      isComplete: false,
+    }]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let toCompleteOneTask = [...tasks];
+    const index = toCompleteOneTask.findIndex(task => task.id === id);
+    toCompleteOneTask[index] = {...toCompleteOneTask[index], isComplete: true};
+
+    setTasks(toCompleteOneTask);
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(
+      tasks.filter(task => task.id !== id)
+    );
   }
 
   return (
